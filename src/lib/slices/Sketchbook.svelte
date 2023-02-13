887bb5cf16235b59f78392bedd96c822c7007ce8
Dimitar Nikolov <49731322+dimitarnikolovv@users.noreply.github.com>
2023-02-13 18:58:25 +0200

    sketxhbook added

--- src/lib/slices/Sketchbook.svelte
+++ src/lib/slices/Sketchbook.svelte
@@ -1,19 +1,28 @@
 <script>
     export let slice;
+
+    const pdfLink = slice.primary.sketchbook_file.url;
+    const apiKey = '05aee1f33099441a';
+
+    let flipbook;
+
+    async function load() {
+        const req = await fetch(
+            `https://heyzine.com/api1/async?pdf=${pdfLink}&k=${apiKey}&tpl=c683c9059ed66a5d378c3489b0bf221f29b043d4.pdf`
+        );
+        flipbook = await req.json();
+    }
+    load();
 </script>
 
-<iframe
-    src={slice.primary.sketchbook_file.url}
-    title="Take a look inside my sketchbook"
-    frameborder="0"
-/>
-
-<pre>{JSON.stringify(slice, null, 2)}</pre>
+{#if flipbook}
+    <iframe title="sketchbook" src={flipbook.url} frameborder="0" />
+{/if}
 
 <style lang="scss">
     iframe {
-        width: 80%;
-        height: 70vh;
-        margin-inline: auto;
+        width: 100%;
+        height: 75vh;
+        max-height: 75vh;
     }
 </style>
