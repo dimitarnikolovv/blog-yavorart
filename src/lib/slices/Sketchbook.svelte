0477c494858045840f5949424e09f403dcdca3f6
Dimitar Nikolov <49731322+dimitarnikolovv@users.noreply.github.com>
2023-02-15 17:38:25 +0200

    idk man

--- src/lib/slices/Sketchbook.svelte
+++ src/lib/slices/Sketchbook.svelte
@@ -1,4 +1,6 @@
 <script>
+    import { error } from '@sveltejs/kit';
+
     export let slice;
 
     const pdfLink =
@@ -13,15 +15,18 @@
         const req = await fetch(
             `https://heyzine.com/api1/async?pdf=${encodeURIComponent(
                 pdfLink
-            )}&k=${encodeURIComponent(apiKey)}`
+            )}&k=${encodeURIComponent(apiKey)}&tpl=333fc4ccd6ad4cd5879f40198dbeeb764e5f1de7.pdf`
         );
-        flipbook = await req.json();
+        if (req.ok) flipbook = await req.json();
+        else error(503, 'A problem occurred getting the sketchbook. Please refresh this page :)');
     })();
 </script>
 
 <div>
-    {#if flipbook && (flipbook?.state == 'processed' ?? false)}
+    {#if flipbook && (flipbook?.state === 'processed' ?? false)}
         <iframe title="sketchbook" src={flipbook.url} frameborder="0" />
+    {:else}
+        <div class="loader">Loading Content...</div>
     {/if}
 </div>
 
@@ -31,10 +36,17 @@
         align-items: center;
         justify-content: center;
     }
+
+    .loader {
+        font-size: 1.4rem;
+    }
     iframe {
-        background-color: white;
         border-radius: 3rem;
         width: 100%;
         height: 75vh;
+
+        @media only screen and (max-width: 780px) {
+            width: 95%;
+        }
     }
 </style>
