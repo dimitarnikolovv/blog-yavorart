634420161efb11f9bdd378a966343e59996773b3
Dimitar Nikolov <49731322+dimitarnikolovv@users.noreply.github.com>
2023-04-05 14:05:49 +0300

    disabled shop

--- src/routes/shop/+layout.server.js
+++ src/routes/shop/+layout.server.js
@@ -3,6 +3,7 @@ import { error } from '@sveltejs/kit';
 import createClient from '$lib/prismicio';
 
 export async function load({ fetch, request }) {
+    return error(404, 'Not found');
     const client = createClient({ fetch, request });
     const shop = await client.getSingle('shop');
 
